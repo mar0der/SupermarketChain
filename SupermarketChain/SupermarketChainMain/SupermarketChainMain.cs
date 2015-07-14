5e780a246030c6e018d68f7bd9b8680fef75fbef
Petar Petkov <petar.b.petkov@gmail.com>
2015-07-14 08:45:39 +0300

    main.cs was added

---
+++ SupermarketChain/SupermarketChainMain/SupermarketChainMain.cs
@@ -0,0 +1,27 @@
+﻿namespace SupermarketChainMain
+{
+    using System;
+    using System.Linq;
+    using System.Runtime.InteropServices;
+
+    public class SupermarketChainMain
+    {
+        public static void Main(string[] args)
+        {
+            using (var db = new Supermarket())
+            {
+                Console.WriteLine("Enter Vendor Name:");
+                var vendorName = Console.ReadLine();
+                var newVendor = new Vendor { VendorName = vendorName };
+                db.Vendors.Add(newVendor);
+                db.SaveChanges();
+                //show all vendors
+                var query = from v in db.Vendors orderby v.VendorName select v;
+                foreach (var vendor in query)
+                {
+                    Console.WriteLine(vendor.VendorName);
+                }
+            }
+        }
+    }
+}
