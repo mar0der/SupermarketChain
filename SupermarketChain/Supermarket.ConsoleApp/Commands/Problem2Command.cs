884888b03e58d9aeb5dc03cdcb2590f33e1989bb
Petar Petkov <petar.b.petkov@gmail.com>
2015-07-22 21:05:47 +0300

    Problem 2 command added. oracle to sql server done

--- SupermarketChain/Supermarket.ConsoleApp/Commands/Problem2Command.cs
+++ SupermarketChain/Supermarket.ConsoleApp/Commands/Problem2Command.cs
@@ -1,18 +1,80 @@
 ﻿namespace Supermarket.ConsoleApp.Commands
 {
+    #region
+
+    using System.Linq;
+
+    using MSSQLDB;
+
+    using MSSQLModels.Models;
+
+    using OracleDB;
+
+    using Supermarket.ConsoleApp.Constants;
     using Supermarket.ConsoleApp.Interfaces;
+
+    #endregion
 
     public class Problem2Command : AbstractCommand
     {
         public Problem2Command(IEngine engine)
-            :base(engine)
+            : base(engine)
         {
         }
 
         public override void Execute()
         {
-            this.Engine.Output.Append("aaa");
+            using (var mssqlContext = new MSSQLContext())
+            using (var oracleContext = new OracleContext())
+            {
+
+                //replicating measures
+                foreach (var measure in oracleContext.MEASURES)
+                {
+                    if (!mssqlContext.Measures.Any(m => m.Name == measure.MEASURE_NAME))
+                    {
+                        mssqlContext.Measures.Add(new Measure { Name = measure.MEASURE_NAME });
+
+                        mssqlContext.SaveChanges();
+                    }
+                }
+
+                this.Engine.Output.AppendLine(Messages.Oracle2MssqlMeasures);
+
+                //replicationg vendors
+                foreach (var vendor in oracleContext.VENDORS)
+                {
+                    if (!mssqlContext.Vendors.Any(v => v.VendorName == vendor.VENDOR_NAME))
+                    {
+                        mssqlContext.Vendors.Add(new Vendor { VendorName = vendor.VENDOR_NAME });
+
+                        mssqlContext.SaveChanges();
+                    }
+                }
+
+                this.Engine.Output.AppendLine(Messages.Oracle2MssqlVendors);
+
+                //replicationg products
+                foreach (var product in oracleContext.PRODUCTS)
+                {
+                    if (!mssqlContext.Products.Any(p => p.ProductName == product.PRODUCT_NAME))
+                    {
+                        mssqlContext.Products.Add(
+                            new Product
+                                {
+                                    ProductName = product.PRODUCT_NAME, 
+                                    Price = product.PRICE, 
+                                    MeasureId = product.MEASURE_ID, 
+                                    VendorId = product.VENDOR_ID
+                                });
+
+                        mssqlContext.SaveChanges();
+                    }
+                }
+
+                this.Engine.Output.AppendLine(Messages.Oracle2MssqlProducts);
+            }
+            this.Engine.Output.AppendLine(Messages.Oracle2MssqlSuccess);
         }
     }
-
 }
--- SupermarketChain/Supermarket.ConsoleApp/Constants/Messages.cs
+++ SupermarketChain/Supermarket.ConsoleApp/Constants/Messages.cs
@@ -3,5 +3,9 @@
     public static class Messages
     {
         public const string InvalidCommand = "Invalid command!";
+        public const string Oracle2MssqlMeasures = "Table Measures Oracle -> MSSQL - Done!";
+        public const string Oracle2MssqlProducts = "Table Products Oracle -> MSSQL - Done!";
+        public const string Oracle2MssqlVendors = "Table Vendors Oracle -> MSSQL - Done!";
+        public const string Oracle2MssqlSuccess = "Oracle data successfully replicated to MSSQL server!";
     }
 }
