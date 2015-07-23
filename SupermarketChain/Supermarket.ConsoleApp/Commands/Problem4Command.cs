e81ffa7bb26bac9531760730e85593a18277f2b0
Gleomit <draychev95@mail.ru>
2015-07-23 17:51:10 +0300

    problem 4 solved, xml working

---
+++ SupermarketChain/Supermarket.ConsoleApp/Commands/Problem4Command.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Xml;
+using MSSQLDB;
+using Supermarket.ConsoleApp.Interfaces;
+
+namespace Supermarket.ConsoleApp.Commands
+{
+    public class Problem4Command : AbstractCommand
+    {
+        public Problem4Command(IEngine engine) : base(engine)
+        {
+        }
+        public override void Execute()
+        {
+            using (var mssqlContext = new MSSQLContext())
+            using (XmlWriter xmlWriter = XmlWriter.Create("Sales-by-Vendors-Report.xml"))
+            {
+                xmlWriter.WriteStartDocument();
+                xmlWriter.WriteStartElement("sales");
+
+                var salesByVendors = from supermarketProduct in mssqlContext.SupermarketProducts
+                    group supermarketProduct by supermarketProduct.Product.VendorId;
+
+
+                foreach (var sale in salesByVendors)
+                {
+                    var vendorName = mssqlContext.Vendors.Find(sale.Key).VendorName;
+                    xmlWriter.WriteStartElement("sale");
+                    xmlWriter.WriteAttributeString("vendor", vendorName); 
+
+                    foreach (var record in sale)
+                    {
+                        xmlWriter.WriteStartElement("summary");
+                        xmlWriter.WriteAttributeString("date", record.SaleDate.ToString("dd-MMMM-yyyy"));
+                        xmlWriter.WriteAttributeString("total-sum", (record.Quantity * record.Product.Price).ToString()); 
+                        xmlWriter.WriteEndElement();
+                    }
+
+                    xmlWriter.WriteEndElement();
+                }
+                
+                xmlWriter.WriteEndElement();
+                xmlWriter.WriteEndDocument();
+            }
+        }
+    }
+}
